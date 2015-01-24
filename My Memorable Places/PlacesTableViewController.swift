e1a92d067781c378298b7773800e479da2b1ba90
Mat Sletten <matsletten@lyellenimac.bbbb.net>
2015-01-23 16:57:41 -0800

    updated code

---
+++ My Memorable Places/PlacesTableViewController.swift
@@ -0,0 +1,114 @@
+//
+//  PlacesTableViewController.swift
+//  My Memorable Places
+//
+//  Created by Mat Sletten on 1/23/15.
+//  Copyright (c) 2015 Mat Sletten. All rights reserved.
+//
+
+import UIKit
+
+var activePlace = 0
+var myPlacesArray = [Dictionary<String,String>()]
+
+class PlacesTableViewController: UITableViewController {
+    
+    //Below we're setting up an array of dictionaries, and the label and values are both strings
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        if myPlacesArray.count == 1 {
+            myPlacesArray.removeAtIndex(0)
+        }
+        myPlacesArray.append(["name":"Taj Mahal","lat":"27.176065","lon":"78.042198"])
+        println(myPlacesArray)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        
+        activePlace = indexPath.row
+        //this allows us to high the default nav bar and use our custom one
+        self.navigationController.navigationBarHidden = true
+        self.performSegueWithIdentifier("findPlace", sender: indexPath)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return myPlacesArray.count
+    }
+
+    
+    override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel.text = myPlacesArray[indexPath.row]["name"]
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView!, moveRowAtIndexPath fromIndexPath: NSIndexPath!, toIndexPath: NSIndexPath!) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView!, canMoveRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- My Memorable Places/ViewController.swift
+++ My Memorable Places/ViewController.swift
@@ -7,12 +7,94 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
+    
+    @IBOutlet weak var myPlacesMap: MKMapView!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    @IBAction func findMeButton(sender: AnyObject) {
+        placeLocation.requestWhenInUseAuthorization()
+        placeLocation.startUpdatingLocation()
+        println(placeLocation)
+    }
+    
+    var placeLocation:CLLocationManager!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let lat = NSString(string: myPlacesArray[activePlace]["lat"]).doubleValue
+        let lon = NSString(string: myPlacesArray[activePlace]["lon"]).doubleValue
+        
+        placeLocation = CLLocationManager()
+        placeLocation.delegate = self
+        placeLocation.desiredAccuracy = kCLLocationAccuracyBest
+        
+        
+        //placesMap setup for picking a starting location
+//        var placeLatitude:CLLocationDegrees = 45.090369
+//        var placeLongitude:CLLocationDegrees = -93.473214
+        
+        //Setup for map to auto center according to which row in tableVC is pressed
+        var myPlaceLat:CLLocationDegrees = lat
+        var myPlaceLon:CLLocationDegrees = lon
+        var placeLatDelta:CLLocationDegrees = 0.01
+        var placeLonDelta:CLLocationDegrees = 0.01
+        var placesMapSpan:MKCoordinateSpan = MKCoordinateSpanMake(placeLatDelta, placeLonDelta)
+        var placesMapLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(myPlaceLat, myPlaceLon)
+        var placesMapRegion:MKCoordinateRegion = MKCoordinateRegionMake(placesMapLocation, placesMapSpan)
+        
+        myPlacesMap.setRegion(placesMapRegion, animated: true)
+        
+        //to add places to the map
+        
+        //add annotations
+        var startLocation = MKPointAnnotation()
+        startLocation.coordinate = placesMapLocation
+        startLocation.title = (string: myPlacesArray[activePlace]["name"])
+        myPlacesMap.addAnnotation(startLocation)
+        
+        //println(activePlace)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        if segue.identifier == "back" {
+            self.navigationController.navigationBarHidden = false
+        }
+    }
+    
+    func locationManager(placeLocation: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var userNewLocation:CLLocation = locations[0] as CLLocation
+        var newLatitudeDT:CLLocationDegrees = userNewLocation.coordinate.latitude
+        var newLongitudeDT:CLLocationDegrees = userNewLocation.coordinate.longitude
+        var newLatDelta:CLLocationDegrees = 0.01
+        var newLongDelta:CLLocationDegrees = 0.01
+        var newMapSpan:MKCoordinateSpan = MKCoordinateSpanMake(newLatDelta, newLongDelta)
+        var newMapLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(newLatitudeDT, newLongitudeDT)
+        var newMapRegion:MKCoordinateRegion = MKCoordinateRegionMake(newMapLocation, newMapSpan)
+        
+        myPlacesMap.setRegion(newMapRegion, animated: true)
+        placeLocation.stopUpdatingLocation()
+        
+        CLGeocoder().reverseGeocodeLocation(userNewLocation, completionHandler:{(placemarks, error) in
+            if (error) {println("Error: \(error)")}
+            else {
+                //println(placemarks)
+                let place = CLPlacemark(placemark: placemarks?[0] as CLPlacemark)
+                var addressNumber:String
+                if (place.subThoroughfare != nil) {
+                    addressNumber = place.subThoroughfare
+                }
+                else {
+                    addressNumber = ""
+                }
+                self.addressLabel.text = "\(place.subLocality)\n\(addressNumber) \(place.thoroughfare)\n\(place.subAdministrativeArea), \(place.administrativeArea)\n\(place.country)"
+            }
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
